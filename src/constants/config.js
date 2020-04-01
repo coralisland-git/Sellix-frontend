0fd892f9b85feac3368aa6200860856185c7bd2d
Knotri <knotri@gmail.com>
2020-04-02 01:08:31 +0300

    Merge branch 'master' of https://github.com/floraiNx/Sellix-Frontend into Max

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ROOT_URL: 'http://51.158.115.106/api/v1',
+  API_ROOT_URL: 'https://staging.sellix.io/api/v1/',
   CAPTCHA_SITE_KEY: '6LdwUeAUAAAAAEe0KlqQT1YaH3Gu18qCm1HFF0Fe'
 }
