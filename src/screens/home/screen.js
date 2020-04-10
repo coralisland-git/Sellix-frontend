b07ac1e07791aaa0176693ddfeb5d9b1d0a04b23
Knotri <knotri@gmail.com>
2020-04-10 16:22:34 +0300

    fix logo in header on landing page

--- src/screens/home/screen.js
+++ src/screens/home/screen.js
@@ -32,7 +32,7 @@ import service2 from 'assets/images/home/service2.svg'
 import service3 from 'assets/images/home/service3.svg'
 import undraw from 'assets/images/home/undraw.svg'
 import undraw1 from 'assets/images/home/undraw_wallet_aym5.svg'
-import sellix_logo from 'assets/images/Sellix_logo.svg'
+import sellix_logo from 'assets/images/Sellix_logo_beta.svg'
 
 class Home extends React.Component {
   
