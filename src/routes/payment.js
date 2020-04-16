4d3598b3e73cb90212876af1f72b69f136fcf911
coralisland-git <coralisland8327@gmail.com>
2020-04-17 05:41:01 +0700

    border radius

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -6,21 +6,21 @@ import {
   } from 'screens'
   
   const paymentRoutes = [
-    {
-      path: '/payment/paypal',
-      name: 'PaypalPay',
-      component: PaypalPaying.screen
-    },
-    {
-      path: '/payment/invoice/:id',
-      name: 'Bitcoin',
-      component: Invoice.screen
-    },
-    {
-      path: '/payment/checkout/:id',
-      name: 'ShopProductDetail',
-      component: ShopProductDetail.screen
-    },
+    // {
+    //   path: '/payment/paypal',
+    //   name: 'PaypalPay',
+    //   component: PaypalPaying.screen
+    // },
+    // {
+    //   path: '/payment/invoice/:id',
+    //   name: 'Bitcoin',
+    //   component: Invoice.screen
+    // },
+    // {
+    //   path: '/payment/checkout/:id',
+    //   name: 'ShopProductDetail',
+    //   component: ShopProductDetail.screen
+    // },
     {
       path: '/payment/embed/:id',
       name: 'EmbededPayment',
