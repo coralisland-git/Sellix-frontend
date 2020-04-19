47473cd81a39230ddee422bf11bc0de34a1ceb3a
coralisland-git <coralisland8327@gmail.com>
2020-04-20 00:02:28 +0800

    Fix Shop Page

--- src/screens/product_shop/screens/detail/screen.js
+++ src/screens/product_shop/screens/detail/screen.js
@@ -315,7 +315,7 @@ class ShopProductDetail extends React.Component {
                       </div>
                     </Card>
                   </Col>
-                  <Col md={4} className="left-bar" >
+                  <Col md={4} className="left-bar" id="affix-bar">
                     <div className="d-sm-down-none" >
                       <Affix offsetTop={97} container='affix-bar'>
                         <Card className="bg-white">
