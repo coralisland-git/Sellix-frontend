43e96c864cc9e5dc8a5580768eec5a6c7f343907
coralisland-git <coralisland8327@gmail.com>
2020-04-28 16:07:37 +0700

    styles

--- src/screens/webhooks/sections/newwebhook_modal.js
+++ src/screens/webhooks/sections/newwebhook_modal.js
@@ -142,8 +142,8 @@ class NewWebhookModal extends React.Component {
                           }}
                           className={
                             props.errors.events && props.touched.events
-                              ? "is-invalid control"
-                              : "control"
+                              ? "is-invalid"
+                              : ""
                           }>
                         </Select>
                         {props.errors.events && props.touched.events && (
@@ -156,7 +156,7 @@ class NewWebhookModal extends React.Component {
                     <Col>
                       <FormGroup>
                         <Label htmlFor="event">{chosenEvents.length} events</Label>
-                        <ul className="chosen-events control">
+                        <ul className="chosen-events">
                           { chosenEvents.map((event, index) => {
                             return(
                               <li key={index} className="d-flex pt-1 pb-1">
