06469a01e45f85291295d9cf73998c6d1ddca2e7
Alexey Savenkov <alexey199753@gmail.com>
2020-04-29 01:31:55 +0300

    google analytics js logs

--- src/layouts/shop/googleAnalytics.js
+++ src/layouts/shop/googleAnalytics.js
@@ -13,11 +13,15 @@ export function GoogleAnalytics(props) {
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${tracking_id}`}></script>
         <script>
             {`
+                console.log('Google Analytics START')
+
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
         
                 gtag('config', '${tracking_id}');
+
+                console.log('Google Analytics END')
             `}
         </script>
     </>
