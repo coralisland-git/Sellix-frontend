96bf240a25c82031318ad2f6c5958bf6872a9ac3
Pavlenko Viktor <pavlenko@reksoft.ru>
2020-04-29 03:27:54 +0300

    Dashboard: Navigation fix;

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -1,111 +1,144 @@
+import { matchPath } from "react-router-dom";
+
 const user = window.localStorage.getItem('userId')
 
-export const mainBrowserNavigation = () => ({
-  items: [
-    {
-      name: 'Dashboard',
-      url: `/dashboard/${user}/home`,
-      icon: 'fas fa-home fa-lg'
-    },
-    {
-      name: 'Products',
-      url: window.location.pathname,
-      icon: 'fas fa-boxes',
-      children: [
-        {
-          name: 'All Products',
-          url: `/dashboard/${user}/products`
-        },
-        {
-          name: 'Groups',
-          url: `/dashboard/${user}/groups`,
-          exact: true
-        },
-        {
-          name: 'Categories',
-          url: `/dashboard/${user}/categories`,
-        },
-        {
-          name: 'Sort Products',
-          url: `/dashboard/${user}/sort/products`,
-        },
-        {
-          name: 'Sort Categories',
-          url: `/dashboard/${user}/sort/categories`,
-        },
-        {
-          name: 'Sort Groups',
-          url: `/dashboard/${user}/sort/groups`,
-          exact: true
-        },
-      ]
-    },
-    {
-      name: 'Orders',
-      url: `/dashboard/${user}/orders`,
-      icon: 'fas fa-credit-card',
-    },
-    {
-      name: 'Analytics',
-      url: `/dashboard/${user}/analytics`,
-      icon: 'fas fa-bar-chart',
-      children: [
-        {
-          name: 'All Analytics',
-          url: `/dashboard/${user}/analytics/stats`,
-        },
-        {
-          name: 'Reports',
-          url: `/dashboard/${user}/analytics/reports`,
-        },
-      ]
-    },
-    {
-      name: 'Coupons',
-      url: `/dashboard/${user}/coupons`,
-      icon: 'fa fa-tags',
-    },
-    {
-      name: 'Queries',
-      url: `/dashboard/${user}/queries`,
-      icon: 'fas fa-question-circle',
-    },
-    {
-      name: 'Feedback',
-      url: `/dashboard/${user}/feedback`,
-      icon: "fa fa-commenting",
-    },
-    {
-      name: 'Blacklist',
-      url: `/dashboard/${user}/blacklist`,
-      icon: 'fas fa-ban',
-    },
-    {
-      name: 'Developer',
-      url: `/dashboard/${user}/developer`,
-      icon: 'fas fa-code',
-      children: [
-        {
-          name: 'Embed Products',
-          url: `/dashboard/${user}/developer/embed-product`,
-        },
-        {
-          name: 'Webhooks',
-          url: `/dashboard/${user}/developer/webhooks`,
-        },
-        {
-          name: 'Webhook Logs',
-          url: `/dashboard/${user}/developer/webhooks/logs`,
-        }
-      ]
-    },
-    // {
-    //   name: 'Pages',
-    //   url: `/dashboard/${user}/pages`,
-    //   icon: 'fa fa-file',
-    // }
+export function mainBrowserNavigation(props){
+
+  let { pathname } = window.location;
+
+  let route = [
+    '/dashboard/:username/groups/new',
+    '/dashboard/:username/groups/edit/:id',
+    '/dashboard/:username/groups',
+    '/dashboard/:username/products/new',
+    '/dashboard/:username/products/edit/:id',
+    '/dashboard/:username/products',
+    '/dashboard/:username/categories/new',
+    '/dashboard/:username/categories/edit/:id',
+    '/dashboard/:username/categories',
+    '/dashboard/:username/sort/groups',
+    '/dashboard/:username/sort/products',
+    '/dashboard/:username/sort/categories',
   ]
-})
+
+  // const match = matchPath(pathname, { path: '/dashboard/:username/groups/:id' })
+  return {
+    items: [
+      {
+        name: 'Dashboard',
+        url: `/dashboard/${user}/home`,
+        icon: 'fas fa-home fa-lg'
+      },
+      {
+        name: 'Products',
+        url:
+            pathname === `/dashboard/${user}/groups` && `/dashboard/${user}/groups` ||
+            pathname.includes(`/dashboard/${user}/groups/new`) && `/dashboard/${user}/groups/new` ||
+            pathname.includes(`/dashboard/${user}/groups/edit`) && `/dashboard/${user}/groups/edit` ||
+            pathname === `/dashboard/${user}/products` && `/dashboard/${user}/products` ||
+            pathname.includes(`/dashboard/${user}/products/new`) && `/dashboard/${user}/products/new` ||
+            pathname.includes(`/dashboard/${user}/products/edit`) && `/dashboard/${user}/products/edit` ||
+            pathname === `/dashboard/${user}/categories` && `/dashboard/${user}/categories` ||
+            pathname.includes(`/dashboard/${user}/categories/new`) && `/dashboard/${user}/categories/new` ||
+            pathname.includes(`/dashboard/${user}/categories/edit`) && `/dashboard/${user}/categories/edit` ||
+            pathname === `/dashboard/${user}/sort/groups` && `/dashboard/${user}/sort/groups` ||
+            pathname === `/dashboard/${user}/sort/products` && `/dashboard/${user}/sort/products` ||
+            pathname === `/dashboard/${user}/sort/categories` && `/dashboard/${user}/sort/categories`,
+        icon: 'fas fa-boxes',
+        children: [
+          {
+            name: 'All Products',
+            url: `/dashboard/${user}/products`
+          },
+          {
+            name: 'Groups',
+            url: `/dashboard/${user}/groups`,
+          },
+          {
+            name: 'Categories',
+            url: `/dashboard/${user}/categories`,
+          },
+          {
+            name: 'Sort Products',
+            url: `/dashboard/${user}/sort/products`,
+          },
+          {
+            name: 'Sort Categories',
+            url: `/dashboard/${user}/sort/categories`,
+          },
+          {
+            name: 'Sort Groups',
+            url: `/dashboard/${user}/sort/groups`,
+          },
+        ]
+      },
+      {
+        name: 'Orders',
+        url: `/dashboard/${user}/orders`,
+        icon: 'fas fa-credit-card',
+      },
+      {
+        name: 'Analytics',
+        url: `/dashboard/${user}/analytics`,
+        icon: 'fas fa-bar-chart',
+        children: [
+          {
+            name: 'All Analytics',
+            url: `/dashboard/${user}/analytics/stats`,
+          },
+          {
+            name: 'Reports',
+            url: `/dashboard/${user}/analytics/reports`,
+          },
+        ]
+      },
+      {
+        name: 'Coupons',
+        url: `/dashboard/${user}/coupons`,
+        icon: 'fa fa-tags',
+      },
+      {
+        name: 'Queries',
+        url: `/dashboard/${user}/queries`,
+        icon: 'fas fa-question-circle',
+      },
+      {
+        name: 'Feedback',
+        url: `/dashboard/${user}/feedback`,
+        icon: "fa fa-commenting",
+      },
+      {
+        name: 'Blacklist',
+        url: `/dashboard/${user}/blacklist`,
+        icon: 'fas fa-ban',
+      },
+      {
+        name: 'Developer',
+        url: `/dashboard/${user}/developer`,
+        icon: 'fas fa-code',
+        children: [
+          {
+            name: 'Embed Products',
+            url: `/dashboard/${user}/developer/embed-product`,
+          },
+          {
+            name: 'Webhooks',
+            url: `/dashboard/${user}/developer/webhooks`,
+          },
+          {
+            name: 'Webhook Logs',
+            url: `/dashboard/${user}/developer/webhooks/logs`,
+          }
+        ]
+      },
+      // {
+      //   name: 'Pages',
+      //   url: `/dashboard/${user}/pages`,
+      //   icon: 'fa fa-file',
+      // }
+    ]
+  }
+}
 
 
 export const mainMobileNavigation = () => ({
