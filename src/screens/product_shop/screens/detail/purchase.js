f3931f1639b4310bad68b075dc60c402b49e0d66
Alexey Savenkov <alexey199753@gmail.com>
2020-05-04 01:17:59 +0300

    feat: handling zero stock and stock < quantity_min products on product page

--- src/screens/product_shop/screens/detail/purchase.js
+++ src/screens/product_shop/screens/detail/purchase.js
@@ -110,26 +110,67 @@ class Purchase extends React.Component {
 
 		if((quantity_max != -1 && count > quantity_max) ||
 		   (quantity_min != -1 && count < quantity_min)) {
-			console.log('qnt_min_max_out_of_bounds', quantity_min, count, quantity_max)
 			return false
 		}
 
 		if((type == 'serials' && stock != -1 && count > stock)) {
-			console.log('stock_out_of_bounds', count, stock)
 			return false
 		}
 
 		if(type == 'file' && file_stock != -1 && count > file_stock) {
-			console.log('stock_out_of_bounds', count, file_stock)
 			return false
 		}
 
 		if(type == 'service' && service_stock != -1 && count > service_stock) {
-			console.log('stock_out_of_bounds', count, service_stock)
 			return false
 		}
 
 		return true;
+	}
+
+	stockUnion = () => {
+		const { type, stock, file_stock, service_stock, quantity_max, quantity_min } = this.props.productInfo;
+
+		console.log('type', type, {
+			'serials': stock,
+			'file': file_stock,
+			'service': service_stock
+		}[type])
+
+		return {
+			'serials': stock,
+			'file': file_stock,
+			'service': service_stock
+		}[type]
+	}
+
+	isOutOfStock = () => {
+		const { type, stock, file_stock, service_stock, quantity_max, quantity_min } = this.props.productInfo;
+
+		const quantityMin = quantity_min == -1 ? 1 : quantity_min
+
+		const stockUnion = this.stockUnion()
+
+		console.log('isOutOfStock', stockUnion, quantityMin, stockUnion < quantityMin)
+
+		return parseInt(stockUnion) < parseInt(quantityMin)
+	}
+
+	outOfStockMessage = () => {
+		const { type, stock, file_stock, service_stock, quantity_max, quantity_min } = this.props.productInfo;
+
+		const stockUnion = this.stockUnion()
+
+		if(stockUnion == 0) {
+			return "Product is not available"
+		}
+
+		const isPlural = stockUnion > 1
+
+		const are = isPlural ? 'are' : 'is'
+		const s = isPlural ? 's' : ''
+
+		return `There ${are} ${stockUnion} item${s} left, but minimum quantity for purchase is ${quantity_min}`
 	}
 
 	setCount = (count) => {
@@ -218,8 +259,9 @@ class Purchase extends React.Component {
 		let { openCoupon, showPaymentOptions, couponSuccess, couponError, couponLoading, appliedCoupon, quantity } = this.state;
 		let currency = config.CURRENCY_LIST[productInfo.currency];
 
-		console.log('quantity', quantity, productInfo)
-		console.log('validCnts', this.isValidCount(quantity - 1), this.isValidCount(quantity + 1))
+		if(this.isOutOfStock() && quantity > 0) {
+			this.setState({ quantity: 0 })
+		}
 
 		return <div>
 			<div className="p-3 pt-2 pb-2">
@@ -233,7 +275,11 @@ class Purchase extends React.Component {
 				<div className="text-center">
 					<h3>{currency}{(productInfo.price_display * quantity * (appliedCoupon ? (100 - appliedCoupon.discount) / 100 : 1)).toFixed(2) || 0}</h3>
 					<div className="mt-3">
-						{!showPaymentOptions && <Button color="primary" className="mr-auto ml-auto d-block" onClick={this.showPaymentOptions} style={{width: 170}}>Purchase</Button>}
+						{!showPaymentOptions && <Button color="primary" className="mr-auto ml-auto d-block" onClick={this.showPaymentOptions} style={this.isOutOfStock() ? {
+							width: 170,
+							opacity: 0.7,
+							pointerEvents: 'none'
+						} : {width: 170}}>Purchase</Button>}
 						{showPaymentOptions && paymentOptions.length === 0 && <p className="mt-3 mb-3 text-grey">This product has no payment options.</p>}
 						<Collapse isOpen={showPaymentOptions}>
 							<div className={paymentOptions.length > 4 ? "d-flex flex-wrap justify-content-between" : ""}>
@@ -241,7 +287,10 @@ class Purchase extends React.Component {
 							</div>
 						</Collapse>
 					</div>
-					<div className="d-flex justify-content-center align-items-center mt-3 stock-count">
+					<div className="d-flex justify-content-center align-items-center mt-3 stock-count" style={this.isOutOfStock() ? {
+						opacity: 0.5,
+						pointerEvents: 'none'
+					} : {}}>
 						<span className={"quantity-picker " + (!this.isValidCount(quantity-1) ? 'text-grey' : '')} style={{ padding: "1rem" }} onClick={this.decreaseCount}><i className="fas fa-minus"/></span>
 						<span style={{ fontSize: 20, minWidth: 25, marginBottom: 2.5 }}>
 			                <input
@@ -254,36 +303,42 @@ class Purchase extends React.Component {
 						<span className={"quantity-picker " + (!this.isValidCount(parseInt(quantity)+1) ? 'text-grey' : '')} onClick={this.increaseCount} style={{ padding: "1rem" }}><i className="fas fa-plus"/></span>
 					</div>
 
-					{openCoupon ?
-						<div className="mt-3">
-							<div style={{display: 'flex'}} className="coupon-apply-container">
-								<Input type="text" id="coupon" name="coupon" placeholder="Coupon code" onChange={(e) => {this.setCoupon(e.target.value)}}/>
-								<Button color="primary" className="mr-auto ml-auto d-block" onClick={this.applyCoupon} style={{
-									width: 100
-								}}>Apply</Button>
-								<style>
-									{`
-										.coupon-apply-container > *:first-child {
-											border-radius: 3px 0 0 3px !important;
-										}
-										.coupon-apply-container > *:nth-child(2) {
-											border-radius: 0 3px 3px 0 !important;
-										}
-									`}
-								</style>
-							</div>
-							<p className="text-grey text-left mt-2 coupon-help" style={{
-								display: 'flex',
-								justifyContent: 'center',
-								paddingTop: '10px'
-							}}>
-								{couponSuccess && <span className="text-green">{parseInt(appliedCoupon.discount)}% Coupon is applied!</span>}
-								{couponError && <span className="text-red">Coupon does not exist or is expired</span>}
-								{/* {couponLoading && Loading()} */}
-							</p>
-						</div> :
-						<p className="text-grey mt-3 cursor-pointer" onClick={this.openCoupon}>Apply a Coupon</p>
-					}
+					{this.isOutOfStock() && <div>
+						<p className="text-red mt-3">{this.outOfStockMessage()}</p>
+					</div>}
+
+					{!this.isOutOfStock() && <>
+						{openCoupon ?
+							<div className="mt-3">
+								<div style={{display: 'flex'}} className="coupon-apply-container">
+									<Input type="text" id="coupon" name="coupon" placeholder="Coupon code" onChange={(e) => {this.setCoupon(e.target.value)}}/>
+									<Button color="primary" className="mr-auto ml-auto d-block" onClick={this.applyCoupon} style={{
+										width: 100
+									}}>Apply</Button>
+									<style>
+										{`
+											.coupon-apply-container > *:first-child {
+												border-radius: 3px 0 0 3px !important;
+											}
+											.coupon-apply-container > *:nth-child(2) {
+												border-radius: 0 3px 3px 0 !important;
+											}
+										`}
+									</style>
+								</div>
+								<p className="text-grey text-left mt-2 coupon-help" style={{
+									display: 'flex',
+									justifyContent: 'center',
+									paddingTop: '10px'
+								}}>
+									{couponSuccess && <span className="text-green">{parseInt(appliedCoupon.discount)}% Coupon is applied!</span>}
+									{couponError && <span className="text-red">Coupon does not exist or is expired</span>}
+									{/* {couponLoading && Loading()} */}
+								</p>
+							</div> :
+							<p className="text-grey mt-3 cursor-pointer" onClick={this.openCoupon}>Apply a Coupon</p>
+						}
+					</>}
 
 				</div>
 			</div>
