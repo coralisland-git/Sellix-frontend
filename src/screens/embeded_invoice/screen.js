5a04541d492737bcce60023b61b169b2adcb2841
Pavlenko Viktor <pavlenko@reksoft.ru>
2020-05-23 02:23:17 +0300

    Remove web sockets from invoices;

--- src/screens/embeded_invoice/screen.js
+++ src/screens/embeded_invoice/screen.js
@@ -85,6 +85,10 @@ class EmbededInvoice extends React.Component {
     document.body.classList.remove('light');
     document.body.classList.remove('dark');
     document.body.classList.add('light');
+
+    document.documentElement.classList.remove('light')
+    document.documentElement.classList.remove('dark')
+    document.documentElement.classList.add('light');
   }
 
   openQrCodeModal = () => {
