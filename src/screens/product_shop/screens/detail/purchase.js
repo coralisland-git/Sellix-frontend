beb6914d42a2c607534d245105ddfc9fd4d103b6
coralisland-git <coralisland8327@gmail.com>
2020-05-23 03:02:49 +0800

    Initialize VPS

--- src/screens/product_shop/screens/detail/purchase.js
+++ src/screens/product_shop/screens/detail/purchase.js
@@ -267,22 +267,16 @@ class Purchase extends React.Component {
 
 		return <div>
 			<div className="p-3 pt-2 pb-2">
-				<div className="d-flex justify-content-between align-items-center mb-3">
-					<h4 className="mt-2 grey">Purchase</h4>
+				{/* <div className="d-flex justify-content-between align-items-center mb-3">
 					<div onClick={this.backToProducts} style={{ cursor: "pointer" }} >
 						<img src={backIcon} className="mb-2" width="15" alt={''}/>
 					</div>
-				</div>
+				</div> */}
 
 				<div className="text-center">
 					{appliedCoupon && <s>{currency}{(productInfo.price_display * quantity).toFixed(2) || 0}</s>}
 					<h3>{currency}{(productInfo.price_display * quantity * (appliedCoupon ? (100 - appliedCoupon.discount) / 100 : 1)).toFixed(2) || 0}</h3>
 					<div className="mt-3">
-						{!showPaymentOptions && <Button color="primary" className="mr-auto ml-auto d-block" onClick={this.showPaymentOptions} style={this.isOutOfStock() ? {
-							width: 170,
-							opacity: 0.7,
-							pointerEvents: 'none'
-						} : {width: 170}}>Purchase</Button>}
 						{showPaymentOptions && paymentOptions.length === 0 && <p className="mt-3 mb-3 text-grey">This product has no payment options.</p>}
 						<Collapse isOpen={showPaymentOptions}>
 							<div className={paymentOptions.length > 4 ? "d-flex flex-wrap justify-content-between" : ""}>
@@ -290,21 +284,8 @@ class Purchase extends React.Component {
 							</div>
 						</Collapse>
 					</div>
-					<div className="d-flex justify-content-center align-items-center mt-3 stock-count" style={this.isOutOfStock() ? {
-						opacity: 0.5,
-						pointerEvents: 'none'
-					} : {}}>
-						<span className={"quantity-picker " + (!this.isValidCount(quantity-1) ? 'text-grey' : '')} style={{ padding: "1rem" }} onClick={this.decreaseCount}><i className="fas fa-minus"/></span>
-						<span style={{ fontSize: 20, minWidth: 25, marginBottom: 2.5 }}>
-			                <input
-				                type="text"
-				                value={quantity}
-				                onChange={(e) => this.setCount(e.target.value)}
-				                onBlur={(e) => this.onBlur(e.target.value)}
-			                />
-		                </span>
-						<span className={"quantity-picker " + (!this.isValidCount(parseInt(quantity)+1) ? 'text-grey' : '')} onClick={this.increaseCount} style={{ padding: "1rem" }}><i className="fas fa-plus"/></span>
-					</div>
+					
+					
 
 					{this.isOutOfStock() ? <div>
 						<p className="text-red mt-3">{this.outOfStockMessage()}</p>
@@ -347,7 +328,29 @@ class Purchase extends React.Component {
 							<p className="text-grey mt-3 cursor-pointer" onClick={this.openCoupon}>Apply a Coupon</p>
 						}
 					</>}
-
+					<div className="d-flex flex-wrap ">
+						<div className="d-flex justify-content-center align-items-center stock-count" style={this.isOutOfStock() ? {
+							opacity: 0.5,
+							pointerEvents: 'none'
+						} : {}}>
+							<span className={"quantity-picker " + (!this.isValidCount(quantity-1) ? 'text-grey' : '')} style={{ padding: "1rem" }} onClick={this.decreaseCount}><i className="fas fa-minus"/></span>
+							<span style={{ fontSize: 20, minWidth: 25, marginBottom: 2.5 }}>
+								<input
+									type="text"
+									value={quantity}
+									onChange={(e) => this.setCount(e.target.value)}
+									onBlur={(e) => this.onBlur(e.target.value)}
+								/>
+							</span>
+							<span className={"quantity-picker " + (!this.isValidCount(parseInt(quantity)+1) ? 'text-grey' : '')} onClick={this.increaseCount} style={{ padding: "1rem" }}><i className="fas fa-plus"/></span>
+						</div>
+						{<Button color="primary" className="mr-auto ml-auto d-block" onClick={this.showPaymentOptions} style={this.isOutOfStock() ? {
+							width: 170,
+							opacity: 0.7,
+							pointerEvents: 'none'
+						} : {width: 170}}>Continue</Button>}
+					</div>
+					
 				</div>
 			</div>
 		</div>
